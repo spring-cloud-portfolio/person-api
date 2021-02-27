afb208ce71ab350d066539804078894c32d09302
Serhey Doroshenko <prigovors@gmail.com>
2021-02-27 16:13:47 +0200

    Implement PersonType.java

---
+++ src/main/java/com/doroshenko/serhey/person/enumeration/person/PersonType.java
@@ -0,0 +1,14 @@
+package com.doroshenko.serhey.person.enumeration.person;
+
+/**
+ * Enumerates possible types of persons
+ *
+ * @author Serhey Doroshenko
+ */
+public enum PersonType {
+    COACH,
+    ATHLETE,
+    REFEREE,
+    INTERNAL_USER,
+    ;
+}
