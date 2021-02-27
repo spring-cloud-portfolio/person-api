b7efa75613ace4ff380cb4c34b7d9cb466b35a4a
Serhey Doroshenko <prigovors@gmail.com>
2021-02-27 17:24:50 +0200

    Add set of person types to PersonDto.java

--- src/main/java/com/doroshenko/serhey/person/dto/person/PersonDto.java
+++ src/main/java/com/doroshenko/serhey/person/dto/person/PersonDto.java
@@ -2,8 +2,11 @@ package com.doroshenko.serhey.person.dto.person;
 
 import com.doroshenko.serhey.person.dto.core.base.BaseDto;
 import com.doroshenko.serhey.person.enumeration.person.Gender;
+import com.doroshenko.serhey.person.enumeration.person.PersonType;
 
 import java.time.LocalDate;
+import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * Data transfer object of person
@@ -18,6 +21,7 @@ public class PersonDto extends BaseDto {
     private String firstName;
     private String middleName;
     private LocalDate birthDay;
+    private Set<PersonType> types;
 
     /* Getters and setters */
     public Gender getGender() {
@@ -58,6 +62,11 @@ public class PersonDto extends BaseDto {
 
     public void setBirthDay(LocalDate birthDay) {
         this.birthDay = birthDay;
+    }
+
+    public Set<PersonType> getTypes() {
+        if (types == null) types = EnumSet.noneOf(PersonType.class);
+        return types;
     }
 
 }
