2ab8af376eb6a6fb5a39ba02f207dab2f252766f
Serhey Doroshenko <prigovors@gmail.com>
2021-06-13 14:23:36 +0300

    Remove InternalUserRegistrationDto.gender field and add InternalUserRegistrationDto.person field

--- src/main/java/com/doroshenko/serhey/person/dto/security/user/registration/InternalUserRegistrationDto.java
+++ src/main/java/com/doroshenko/serhey/person/dto/security/user/registration/InternalUserRegistrationDto.java
@@ -1,5 +1,6 @@
 package com.doroshenko.serhey.person.dto.security.user.registration;
 
+import com.doroshenko.serhey.person.dto.person.PersonDto;
 import com.doroshenko.serhey.person.enumeration.person.Gender;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -15,21 +16,21 @@ import java.io.Serializable;
 public class InternalUserRegistrationDto implements Serializable {
 
     private final String email;
-    private final Gender gender;
     private final String username;
     private final String password;
+    private final PersonDto person;
     private final String mobilePhone;
     private final String passwordConfirmation;
 
     @JsonCreator
     public InternalUserRegistrationDto(@JsonProperty("email") String email,
-                                       @JsonProperty("gender") Gender gender,
                                        @JsonProperty("username") String username,
                                        @JsonProperty("password") String password,
+                                       @JsonProperty("person") PersonDto person,
                                        @JsonProperty("mobilePhone") String mobilePhone,
                                        @JsonProperty("passwordConfirmation") String passwordConfirmation) {
         this.email = email;
-        this.gender = gender;
+        this.person = person;
         this.username = username;
         this.password = password;
         this.mobilePhone = mobilePhone;
@@ -41,16 +42,16 @@ public class InternalUserRegistrationDto implements Serializable {
         return email;
     }
 
-    public Gender getGender() {
-        return gender;
-    }
-
     public String getUsername() {
         return username;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public PersonDto getPerson() {
+        return person;
     }
 
     public String getMobilePhone() {
